6e8bd0869b98001756b07bdde8ca6ae9c4e4313c
iyidaclem <iyidaclem@gmail.com>
2022-12-04 21:07:29 +0100

    check for palindrome in a linked list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,31 @@
 #include "lists.h"
 
-/**
+/*
  * check_cycle - checks if a singly linked list has
  * a cycle in it
  * @list: pointer to the list
  * Return: 0 if there is no cycle,
  * 1 if there is a cycle
  */
+/*
+*int check_cycle(listint_t *list)
+*{
+*	listint_t *f;
+*	listint_t *s;
+*
+*	f = list;
+*	s = list;
+*	while (s && f && f->next)
+*	{
+*		s = s->next;
+*		f = f->next->next;
+*		if (s == f)
+*			return (1);
+*	}
+*	return (0);
+*}
+*/
 
-int check_cycle(listint_t *list)
-{
-	listint_t *f;
-	listint_t *s;
-
-	f = list;
-	s = list;
-	while (s && f && f->next)
-	{
-		s = s->next;
-		f = f->next->next;
-		if (s == f)
-			return (1);
-	}
-	return (0);
-}
 /**
 * is_palindrome - Checks if a linked list is a palindrome
 * @head: the head of the linked list
@@ -38,7 +40,7 @@ int is_palindrome(listint_t **head)
 
 	if (!head || (*head)->next == NULL)
 		return (1);
-	if (check_cycle(*head))
+/*	if (check_cycle(*head))*/
 		return (1);
 	list_vals = (int *)malloc(sizeof(int) * 1);
 	if (list_vals == NULL)
