358bd105a6a0e245dd346fe627c04f15aed487df
iyidaclem <iyidaclem@gmail.com>
2022-12-08 00:49:37 +0100

    roman numeral converter

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string:
-        return None
+        return 0
     roman_table = {"I": 1, "V": 5, "X": 10,
                    "L": 50, "C": 100, "D": 500,
                    "M": 1000}
@@ -17,5 +17,5 @@ def roman_to_int(roman_string):
                 total += roman_table[j]
                 prev = roman_table[j]
         else:
-            return None
+            return 0
     return total
