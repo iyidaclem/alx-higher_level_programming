b600922a15fa2b60290e9f30d58138793c15b2a5
iyidaclem <iyidaclem@gmail.com>
2022-12-08 02:35:26 +0100

    roman numeral converter

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
+import re
+
+
 def roman_to_int(roman_string):
     if not roman_string:
+        return 0
+    pattern = re.compile(r"""
+                          ^M{0,3}
+                          (CM|CD|D?C{0,3})?
+                          (XC|XL|L?X{0,3})?
+                          (IX|IV|V?I{0,3})?$
+            """, re.VERBOSE)
+    if re.match(pattern, roman_string.upper()):
+        pass
+    else:
         return 0
     roman_table = {"I": 1, "V": 5, "X": 10,
                    "L": 50, "C": 100, "D": 500,
                    "M": 1000}
     total = 0
     prev = 0
+    rep = 0
     roman_string = roman_string.upper()
     for j in roman_string:
         if j in roman_table:
