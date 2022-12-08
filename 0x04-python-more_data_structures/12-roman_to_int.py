16dfe0afe2c85a1517887d1815b2767128b88412
iyidaclem <iyidaclem@gmail.com>
2022-12-08 02:46:54 +0100

    saved

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python3
-import re
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return (max_list - to_sub)
 
 
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
-    pattern = re.compile(r"""
-                          ^M{0,3}
-                          (CM|CD|D?C{0,3})?
-                          (XC|XL|L?X{0,3})?
-                          (IX|IV|V?I{0,3})?$
-            """, re.VERBOSE)
-    if re.match(pattern, roman_string.upper()):
-        pass
-    else:
+
+    if not isinstance(roman_string, str):
         return 0
-    roman_table = {"I": 1, "V": 5, "X": 10,
-                   "L": 50, "C": 100, "D": 500,
-                   "M": 1000}
-    total = 0
-    prev = 0
-    rep = 0
-    roman_string = roman_string.upper()
-    for j in roman_string:
-        if j in roman_table:
-            if prev < roman_table[j]:
-                total += roman_table[j] - (prev * 2)
-                prev = roman_table[j]
-            else:
-                total += roman_table[j]
-                prev = roman_table[j]
-        else:
-            return 0
-    return total
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
+
+                last_rom = rom_n.get(ch)
+
+    num += to_subtract(list_num)
+
+    return (num)
