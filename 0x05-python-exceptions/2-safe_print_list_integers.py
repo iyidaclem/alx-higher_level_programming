866b5d575ea351d16b2b45c2fdf24779cf821518
iyidaclem <iyidaclem@gmail.com>
2022-12-19 22:43:40 +0100

    print only integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
+
+def safe_print_integer(value):
     """Print integer with "{:d}".format().
     Args:
         value (int): Integer to print.
@@ -11,4 +12,3 @@ def safe_print_list_integers(my_list=[], x=0):
         print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
-        return (False)
