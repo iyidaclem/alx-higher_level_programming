284461b9fc1d4d14a96809b4d3f2b126d92fb514
iyidaclem <iyidaclem@gmail.com>
2023-01-09 16:36:33 +0100

    check if it is instance of

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a function that check if if object is instance of a class"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if obj is instance of a_class
+
+    Args:
+        object: The object in question
+        a_class: The class in question
+    Returns: True if is instance and false if not
+    """
+    return isinstance(obj, a_class)
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -5,3 +5,6 @@ attributes and methods of an object
 
 ## [`1-my_list.py`](1-my_list.py)
 Write a class MyList that inherits from list
+
+## [`2-is_same_class.py`](2-is_same_class.py)
+Check instance
