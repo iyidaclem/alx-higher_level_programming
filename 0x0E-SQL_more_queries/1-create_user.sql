77ff4cf1fe836b3cc2a99cbdd3c7d0cbdd77aa00
iyidaclem <iyidaclem@gmail.com>
2023-02-15 22:45:00 +0100

    create user with password

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,2 @@
+-- Write sql that creates mysql server user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
--- 0x0E-SQL_more_queries/README.md
+++ 0x0E-SQL_more_queries/README.md
@@ -3,3 +3,6 @@
 
 ## [`0-privileges.sql`](0-privileges.sql)
 Show privileges for users
+
+## [`1-create_user.sql`](1-create_user.sql)
+Create mysql user
