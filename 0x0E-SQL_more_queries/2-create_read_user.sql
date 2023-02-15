be19228e83aa8a6e7d1c2cd1c0bf974cb85b2394
iyidaclem <iyidaclem@gmail.com>
2023-02-15 22:56:25 +0100

    Create database and new user with SELECT permission

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Create databasee and new user and grant SELECT permision to the user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GREANT SELECT TO 'user_0d_2'@'localhost' ON hbtn_0d_2;
--- 0x0E-SQL_more_queries/README.md
+++ 0x0E-SQL_more_queries/README.md
@@ -6,3 +6,6 @@ Show privileges for users
 
 ## [`1-create_user.sql`](1-create_user.sql)
 Create mysql user
+
+## [`2-create_read_user.sql`](2-create_read_user.sql)
+Create database and user and grant SELECT to new user
