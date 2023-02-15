d1b676381c96cb0227971485fcfc8e5b18a9c3cf
iyidaclem <iyidaclem@gmail.com>
2023-02-15 22:47:57 +0100

    create user with password

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- Write sql that creates mysql server user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
+IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGE TO 'user_0d_1'@'localhost';
