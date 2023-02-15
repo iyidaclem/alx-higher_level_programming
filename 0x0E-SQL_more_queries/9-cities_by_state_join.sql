2d0f3bb7af5cd7769d1c1cbc3da410aed52cf726
iyidaclem <iyidaclem@gmail.com>
2023-02-16 00:51:27 +0100

    Lists cities by states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
---Cities by States
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
+-- Cities by States
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC;
