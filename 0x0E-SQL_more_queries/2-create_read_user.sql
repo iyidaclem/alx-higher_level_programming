a6a2ccacc4297e4de35730a231983b140317b6c3
iyidaclem <iyidaclem@gmail.com>
2023-02-15 23:02:01 +0100

    Create database and new user with SELECT permission

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- Create databasee and new user and grant SELECT permision to the user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- Create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT TO 'user_0d_2'@'localhost' ON hbtn_0d_2.*;
+-- grant user permision
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
