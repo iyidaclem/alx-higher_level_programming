166b757922a6e7a7103c479d89340f4f31546c0d
iyidaclem <iyidaclem@gmail.com>
2023-02-16 00:19:01 +0100

    cities in a state

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,6 @@
 -- Select all the cities of california
-SELECT id, cities.name
-FROM states, cities
-WHERE cities.state_id = states.id;
+SELECT id, name
+FROM cities
+WHERE id = (
+    SELECT id states where name  = 'California')
+ORDER BY id desc;
