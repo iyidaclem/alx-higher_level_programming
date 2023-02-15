01f5b47ecd807c1a44b8dddd21b8e68ce9be44d5
iyidaclem <iyidaclem@gmail.com>
2023-02-15 22:38:48 +0100

    show privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Show privileges for users
-SHOW PRIVILEGE FOR 'user_0d_1'@'localhost';
-SHOW PRIVILEGE FOR 'user_0d_2'@'localhost';
+-- Show all privileges of MySQL users user_0d_1 and user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
