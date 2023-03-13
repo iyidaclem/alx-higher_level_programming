da4b42d30543a53457d6ea1fd8d882084a54ea65
iyidaclem <iyidaclem@gmail.com>
2023-03-13 23:34:40 +0100

    Iteration

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-if (process.argv[2] !== undefined) {
-  if (isNaN(process.argv[2])) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = process.argv[2]; i > 0; i--) console.log('C is fun');
-  }
-} else {
+if (isNaN(process.argv[2]) || (process.argv[2] === undefined)) {
   console.log('Missing number of occurrences');
+} else {
+  for (let i = process.argv[2]; i > 0; i--) console.log('C is fun');
 }
