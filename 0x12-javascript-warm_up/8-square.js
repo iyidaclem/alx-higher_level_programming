1328dc44e55bc02f3da6722e1dcd068b46e01d1a
iyidaclem <iyidaclem@gmail.com>
2023-03-13 23:54:44 +0100

    Print square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || (process.argv[2] === undefined)) {
+  console.log('Missing size');
+} else {
+  for (let i = process.argv[2]; i > 0; i--) {
+    let str = '';
+    for (let j = 0; j < process.argv[2]; j++) str += '#';
+    console.log(str);
+  }
+}
