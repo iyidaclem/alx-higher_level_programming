cec945094661a5b8e165ebd85f1bcbb8da8a6cc6
iyidaclem <iyidaclem@gmail.com>
2023-03-14 00:07:33 +0100

    Square printer updated

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ if (isNaN(process.argv[2]) || (process.argv[2] === undefined)) {
 } else {
   for (let i = process.argv[2]; i > 0; i--) {
     let str = '';
-    for (let j = 0; j < process.argv[2]; j++) str += '#';
+    for (let j = 0; j < process.argv[2]; j++) str += 'X';
     console.log(str);
   }
 }
