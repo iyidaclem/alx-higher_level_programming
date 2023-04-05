1c1d5490720d44adb5958ba0f4c82e9fda438eae
iyidaclem <iyidaclem@gmail.com>
2023-04-05 08:48:25 +0000

    Send data through curl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send post request through curl
-curl -s $1 -X POST -d "email=test@gmail&subject=I will always be here for PLD"
+curl -s "$1" -X POST -d "email=test@gmail&subject=I will always be here for PLD"
