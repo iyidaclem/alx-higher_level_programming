a9942d9496298af17b4dbd5615ebdbfed87d3153
iyidaclem <iyidaclem@gmail.com>
2023-04-07 15:02:35 +0100

    Http response code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """find the peak of a list"""
 # a bash script that print the response code from http request
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -so /dev/null -w "%{http_code}" "$1"
