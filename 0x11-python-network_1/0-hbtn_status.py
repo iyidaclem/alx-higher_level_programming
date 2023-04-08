73a8c8bdff5b0b41c23092361c46b50681a3cc29
iyidaclem <iyidaclem@gmail.com>
2023-04-08 09:27:15 +0100

    saved

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Python script that fetches from the url https://intranet.hbtn.io/status """
 from urllib import request
+from urllib.request import urlopen
 
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
+    with urlopen("https://intranet.hbtn.io/status") \
+                 as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
