8ec59ee41dc594b9c3e2b29fd78319a4f8d9cbdd
iyidaclem <iyidaclem@gmail.com>
2023-04-08 10:12:14 +0100

    script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,4 +9,5 @@ import requests
 if __name__ == "__main__":
     res = requests.get('https://intranet.hbtn.io/status')
     txt = res.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(txt), txt))
+    print('Body response:\n\t- type: {}\n\t- content: {}'
+          .format(type(txt), txt))
